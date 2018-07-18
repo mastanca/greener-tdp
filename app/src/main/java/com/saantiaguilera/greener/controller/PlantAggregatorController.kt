9bb8420aafcaf7a26d13772e731d0e9ae390bdce
Manuel Porto <manu.porto94@gmail.com>
2018-07-17 21:41:19 -0300

    Fix input

--- app/src/main/java/com/saantiaguilera/greener/controller/PlantAggregatorController.kt
+++ app/src/main/java/com/saantiaguilera/greener/controller/PlantAggregatorController.kt
@@ -79,53 +79,15 @@ class PlantAggregatorController : RxController(), LayoutContainer {
         var valid = true
 
         val name = inputName.text.toString()
-        val address = inputAddress.text.toString()
-        val email = inputEmail.text.toString()
-        val mobile = inputMobile.text.toString()
-        val password = inputPassword.text.toString()
-        val reEnterPassword = inputReEnterPassword.text.toString()
+        val temperature = inputTemperature.text.toString().toDouble()
+        val dailySunlightHours = inputDialySunlightHours.text.toString().toDouble()
+        val wateringInterval = inputWateringInterval.text.toString().toInt()
 
         if (name.isEmpty() || name.length < 3) {
             inputName.error = "at least 3 characters"
             valid = false
         } else {
             inputName.error = null
-        }
-
-        if (address.isEmpty()) {
-            inputAddress.error = "Enter Valid Address"
-            valid = false
-        } else {
-            inputAddress.error = null
-        }
-
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            inputEmail.error = "enter a valid email address"
-            valid = false
-        } else {
-            inputEmail.error = null
-        }
-
-        if (mobile.isEmpty() || mobile.length != 10) {
-            inputMobile.error = "Enter Valid Mobile Number"
-            valid = false
-        } else {
-            inputMobile.error = null
-        }
-
-        if (password.isEmpty() || password.length < 4 || password.length > 10) {
-            inputPassword.error = "between 4 and 10 alphanumeric characters"
-            valid = false
-        } else {
-            inputPassword.error = null
-        }
-
-        if (reEnterPassword.isEmpty() || reEnterPassword.length < 4 || reEnterPassword.length > 10 || reEnterPassword != password) {
-            inputReEnterPassword.error = "Password Do not match"
-            valid = false
-        } else {
-            inputReEnterPassword.error = null
         }
 
         return valid
